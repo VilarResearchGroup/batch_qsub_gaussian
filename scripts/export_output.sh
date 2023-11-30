2067d831e375a540ff4c7c0dded52a7847299519
Wilson <wilsonweiwu@gmail.com>
2023-11-30 16:11:06 +0000

    Added extra --include option to grep to search for .log and .LOG files

--- scripts/export_output.sh
+++ scripts/export_output.sh
@@ -4,8 +4,8 @@ NORM=norm_term
 ERR=err_term
 mkdir $NORM $ERR
 
-grep -rl "Normal termination" --include="*.log" |  xargs -I{} cp "{}" ${NORM}
-grep -rl "Error termination" --include="*.log" |  xargs -I{} cp "{}" ${ERR}
+grep -rl "Normal termination" --include="*.log" --include="*.LOG" |  xargs -I{} cp "{}" ${NORM}
+grep -rl "Error termination" --include="*.log" --include="*.LOG" |  xargs -I{} cp "{}" ${ERR}
 
 NORM_COUNT=$(find ${NORM} -type f -name '*.log' | wc -l)
 ERR_COUNT=$(find ${ERR} -type f -name '*.log' | wc -l)
